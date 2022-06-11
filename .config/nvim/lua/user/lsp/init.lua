bf46f0338c6bef768b0291087fce69a7aa6b34f3
Jordi Coll <jjordicoll@gmail.com>
2022-06-11 12:09:50 +0200

    first go at adding lsp

---
+++ .config/nvim/lua/user/lsp/init.lua
@@ -0,0 +1,8 @@
+-- Python server
+require'lspconfig'.pyright.setup{}
+
+-- Html server
+require'lspconfig'.html.setup{}
+
+-- Css Server
+require'lspconfig'.cssls.setup{}
--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -61,15 +61,16 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "hrsh7th/cmp-nvim-lua" -- nvim config completion
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions 
+  use "hrsh7th/cmp-nvim-lsp" -- LSP completions for cmp
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- LSP
-  -- use "neovim/nvim-lspconfig" -- enable LSP
-  -- use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
