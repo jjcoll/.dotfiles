2d212386b72185b84611634083b7e4f29b7df8bf
Jordi Coll <jjordicoll@gmail.com>
2022-06-11 12:21:24 +0200

    fixed css autocompletion

--- .config/nvim/lua/user/lsp/init.lua
+++ .config/nvim/lua/user/lsp/init.lua
@@ -1,8 +1,17 @@
+-- Enable autocompletion 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 -- Python server
 require'lspconfig'.pyright.setup{}
 
 -- Html server
-require'lspconfig'.html.setup{}
+require'lspconfig'.html.setup {
+  capabilities = capabilities,
+}
 
 -- Css Server
-require'lspconfig'.cssls.setup{}
+require'lspconfig'.cssls.setup{
+  capabilities = capabilities,
+}
+-- require'lspconfig'.cssmodules_ls.setup{}
