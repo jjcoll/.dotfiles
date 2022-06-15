eecacc6458b5c718ff7379a295548e2c69b70a77
Jordi Coll <jjordicoll@gmail.com>
2022-06-15 15:10:44 +0200

    fixed lsp folder structure

---
+++ .config/nvim/lua/user/lsp/lsp-installer.lua
@@ -0,0 +1,11 @@
+-- First require Lsp Installer, this config is the defaul found in: https://github.com/williamboman/nvim-lsp-installer#configuration
+require("nvim-lsp-installer").setup({
+    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+    ui = {
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    }
+})
